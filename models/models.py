041250d06ff2c9c8f34350cde197b39f45c156de
loreski89 <lorenzo.schivo@me.com>
2024-10-18 21:28:56 +0200

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,64 +1,47 @@
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor
-from keras.models import Sequential
-from keras.layers import LSTM, Dense
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
 
-# Modello LSTM
 class LSTMModel:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, stock_data):
+        self.stock_data = stock_data
+        self.model = None
+        self.scaler = MinMaxScaler()
 
     def preprocess_data(self, macro_data, sentiment_data):
-        # Preprocessa i dati storici e macroeconomici
-        pass
+        # Preprocessing: Scaling and feature engineering with macro and sentiment data
+        self.stock_data['Close'] = self.scaler.fit_transform(self.stock_data['Close'].values.reshape(-1, 1))
+        # Aggiungi altre feature come macro_data e sentiment_data
 
-    def create_sequences(self):
-        # Crea le sequenze per il training del modello
-        pass
+    def create_sequences(self, sequence_length=60):
+        X, y = [], []
+        for i in range(sequence_length, len(self.stock_data['Close'])):
+            X.append(self.stock_data['Close'][i-sequence_length:i])
+            y.append(self.stock_data['Close'][i])
+        return np.array(X), np.array(y)
 
     def build_model(self):
         self.model = Sequential()
-        self.model.add(LSTM(50, return_sequences=True))
+        self.model.add(LSTM(units=50, return_sequences=True, input_shape=(60, 1)))
+        self.model.add(Dropout(0.2))
+        self.model.add(LSTM(units=50))
+        self.model.add(Dropout(0.2))
         self.model.add(Dense(1))
+
         self.model.compile(optimizer='adam', loss='mean_squared_error')
 
-    def train_model(self, X_train, y_train):
-        self.model.fit(X_train, y_train, epochs=10, batch_size=64)
+    def train_model(self, X_train, y_train, epochs=10, batch_size=32):
+        self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
 
     def predict_future(self, projection_range):
-        # Previsione futura
-        return np.random.randn(30)  # Previsione casuale per esempio
+        # Implementa la logica per la previsione futura
+        return self.model.predict(self.stock_data[-60:].reshape(1, 60, 1))
 
-# Modello Transformer (placeholder, da implementare)
 class TransformerModel:
-    def __init__(self, data):
-        self.data = data
-    
-    def preprocess_data(self, macro_data, sentiment_data):
+    def __init__(self, stock_data):
+        self.stock_data = stock_data
+        # Implementa un modello Transformer o un altro tipo di modello avanzato
         pass
-    
-    def create_sequences(self):
-        pass
-    
-    def build_model(self):
-        pass
-    
-    def train_model(self, X_train, y_train):
-        pass
-    
-    def predict_future(self, projection_range):
-        return np.random.randn(30)
 
-# Modello Random Forest
-class RandomForestModel:
-    def __init__(self, data):
-        self.model = RandomForestRegressor()
-    
-    def preprocess_data(self, macro_data, sentiment_data):
-        pass
-    
-    def train_model(self, X_train, y_train):
-        self.model.fit(X_train, y_train)
-
-    def predict_future(self, projection_range):
-        return np.random.randn(30)  # Previsione casuale per esempio

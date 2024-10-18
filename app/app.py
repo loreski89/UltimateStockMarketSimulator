947f0004fd7369e081717716a94c8479c308eca1
loreski89 <lorenzo.schivo@me.com>
2024-10-18 21:18:26 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
-
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
